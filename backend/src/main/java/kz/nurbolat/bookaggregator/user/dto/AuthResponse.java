9b6cabd5a0aac4a5341f48b43fb19f2204cfc700
nurbolat <djumadilovnurik9@gmail.com>
2026-06-05 18:23:06 +0500

    add auth endpoints for register and login

---
+++ backend/src/main/java/kz/nurbolat/bookaggregator/user/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package kz.nurbolat.bookaggregator.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String email;
+    private String name;
+}
