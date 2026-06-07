9719c5b89e12494a67f0254985306262017a8eba
nurbolat <djumadilovnurik9@gmail.com>
2026-06-07 16:49:06 +0500

    add async thread pool configuration

---
+++ backend/src/main/java/kz/nurbolat/bookaggregator/config/AsyncConfig.java
@@ -0,0 +1,22 @@
+package kz.nurbolat.bookaggregator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("book-async-");
+        executor.initialize();
+        return executor;
+    }
+}
